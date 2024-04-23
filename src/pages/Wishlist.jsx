547e73bc49876409c371f3516e36b97fd056c6b9
ayanshuDS <73418468+MET4L-DS@users.noreply.github.com>
2024-04-23 23:50:52 +0530

    last changes

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -7,7 +7,7 @@ import { FaLeaf } from "react-icons/fa6";
 const Wishlist = () => {
     const { wishlistItems } = useSelector((state) => state.wishlist);
     return (
-        <div className="col-[2/-2] grid grid-cols-subgrid py-20">
+        <div className="col-[1/-1] grid grid-cols-subgrid gap-4 px-6 py-20 font-semibold sm:col-[2/-2] sm:px-0">
             <SectionHeader
                 icon={FaLeaf}
                 tagColor="green-500"
